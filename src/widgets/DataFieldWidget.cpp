10156f0a875761f524b0ba2e9445de9ce8234feb
foxtrot <felix.wieser@tum.de>
2018-01-22 12:43:11 +0100

    DataFieldWidget implementation

--- src/widgets/DataFieldWidget.cpp
+++ src/widgets/DataFieldWidget.cpp
@@ -1,3 +1,5 @@
+#include <sstream>
+#include <math.h>
 #include "DataFieldWidget.hpp"
 #include "../MainWindow.hpp"
 
@@ -21,23 +23,29 @@ DataFieldWidget* DataFieldWidget::create(swegui::MainWindow* pa, std::string na)
 
 void DataFieldWidget::setup_gui_elements()
 {
+    //Raw data tab
+    for(int i = 0; i < 5; i++)
+        m_refBuilder->get_widget("lbl_"+layer_names[i], labels[i]);
+    
+    //Graph tab
+    m_refBuilder->get_widget("cb_layer", cb_layer);
 
+    update_ui();
 }
 
 void DataFieldWidget::update_ui()
 {
+    std::cout << "Updating ui" << std::endl;
     if(name != "")
     {
-        //put data from parent->data_renderer->probes[name] into ui elements
-        //use coordinates from probe
-        //sample data with parent->data_renderer->sample
-        //does not work for hx (not in file), calculate by hand
-        //note: sample is not yet implemented, always returns 0
-
-        //tip: put controls in arrays and use parent->data_renderer->layers instead of individual objects
-        //check out and adapt implementation from LayerWindow
-
-        //when i think of it, this class should not be very different from LayerWindow
-        //it even has only uni- instead of bi-directional dataflow
+        probe::DataProbe* probe = &(parent->data_renderer->probes[name]);
+        //Compute hx
+        float hx = std::abs(std::sqrt(std::pow(parent->data_renderer->sample(renderer::NetCdfImageStream::Variable::Hu, probe->x, probe->y), 2)
+            + std::pow(parent->data_renderer->sample(renderer::NetCdfImageStream::Variable::Hu, probe->x, probe->y), 2)));
+        labels[0]->set_text(std::to_string(parent->data_renderer->sample(renderer::NetCdfImageStream::Variable::B, probe->x, probe->y)));
+        labels[1]->set_text(std::to_string(parent->data_renderer->sample(renderer::NetCdfImageStream::Variable::H, probe->x, probe->y)));
+        labels[2]->set_text(std::to_string(parent->data_renderer->sample(renderer::NetCdfImageStream::Variable::Hu, probe->x, probe->y)));
+        labels[3]->set_text(std::to_string(parent->data_renderer->sample(renderer::NetCdfImageStream::Variable::Hv, probe->x, probe->y)));
+        labels[4]->set_text(std::to_string(hx));
     }
 }
