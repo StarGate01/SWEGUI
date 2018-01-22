5c225246814c44b90eb819fb752a0aa8ff729551
foxtrot <felix.wieser@tum.de>
2018-01-22 16:50:03 +0100

    Minor code formatting

--- src/Application.hpp
+++ src/Application.hpp
@@ -4,7 +4,6 @@
 #include <iostream>
 #include <gtkmm.h>
 #include "MainWindow.hpp"
-#include "GuiEnums.hpp"
 
 namespace swegui
 {
@@ -13,22 +12,17 @@ namespace swegui
     {
 
         public:
-
             static Glib::RefPtr<Application> create(); 
 
           
         protected:
-
             Application();
             void on_activate() override;
             void on_open(const Gio::Application::type_vec_files& files, const Glib::ustring& hint) override;
 
         private:
-
             MainWindow* create_appwindow();
             void on_hide_window(Gtk::Window* window);
-            void on_window_action(Signal signal);
-
     };
 
 }
