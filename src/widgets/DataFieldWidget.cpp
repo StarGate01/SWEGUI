36ad6c7a149462f560b56c17e43b33dc53765324
foxtrot <felix.wieser@tum.de>
2018-01-23 15:57:00 +0100

    Intermediate commit for graph implementation

--- src/widgets/DataFieldWidget.cpp
+++ src/widgets/DataFieldWidget.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <sstream>
 #include <math.h>
+#include <vector>
+#include <limits>
 #include "DataFieldWidget.hpp"
 #include "../MainWindow.hpp"
 
@@ -29,10 +31,11 @@ void DataFieldWidget::setup_gui_elements()
         m_refBuilder->get_widget("lbl_"+layer_names[i], labels[i]);
     
     m_refBuilder->get_widget("cb_layer", cb_layer);
+    m_refBuilder->get_widget("liststore_cb", cb_layer_model);
     m_refBuilder->get_widget("drawingarea_chart", drawingarea_chart);
 
     drawingarea_chart->signal_draw().connect(sigc::mem_fun(this, &DataFieldWidget::on_chart_draw));
-    //todo bind combobox change event
+    cb_layer->signal_changed().connect(sigc::mem_fun(this, &DataFieldWidget::on_dataset_change));           //TODO: Add parameter to function
 
     update_ui();
 }
@@ -40,6 +43,7 @@ void DataFieldWidget::setup_gui_elements()
 void DataFieldWidget::populate_data()
 {
     //todo fill presampled array thingy
+    
 }
 
 void DataFieldWidget::update_ui()
@@ -53,7 +57,7 @@ void DataFieldWidget::update_ui()
 
         //Compute hx
         float hx = std::abs(std::sqrt(std::pow(parent->data_renderer->sample(renderer::NetCdfImageStream::Variable::Hu, probe->x, probe->y), 2)
-            + std::pow(parent->data_renderer->sample(renderer::NetCdfImageStream::Variable::Hu, probe->x, probe->y), 2)));
+            + std::pow(parent->data_renderer->sample(renderer::NetCdfImageStream::Variable::Hv, probe->x, probe->y), 2)));
         labels[4]->set_text(std::to_string(hx));
     }
     else
@@ -68,9 +72,69 @@ void DataFieldWidget::reset_gui()
         label->set_text("n/a");
 }
 
-bool DataFieldWidget::on_chart_draw(const Cairo::RefPtr<Cairo::Context>& cr)
+void DataFieldWidget::on_dataset_change(void)
 {
-    //todo use cr to draw graph, but only if data array is filled
-    //based on what is selected in combobox
+    //TODO: Call DataFieldWidet::on_chart_draw
+    std::cout << "Selected " << cb_get_selected_layer() << std::endl;
+}
+
+bool DataFieldWidget::on_chart_draw(const Cairo::RefPtr<Cairo::Context>& cr)
+{ 
+    probe::DataProbe* probe = &(parent->data_renderer->probes[name]);
+    //Check if dataset is filled
+    if(!probe->has_data())
+        return true;
+    
+    //TODO: Calculate graph width and height
+    int width = 0;
+    int height = 0;
+    int layer = cb_get_selected_layer();
+    std::vector<float> data = probe->get_all_data(layer);
+
+    if(data.size() <= 0)
+        return true;
+
+    float max_value = std::numeric_limits<float>::min();
+    //Get maximum element
+    for(auto const& val : data)
+    {
+        if(max_value < val)
+            max_value = val;
+    }
+
+    if(data.size() == 1)
+    {
+        cr->move_to(calculate_graph_width(0, static_cast<int>(data.size()), width), height/2);
+        cr->line_to(width, height/2);
+        return true;
+    }
+
+    cr->move_to(calculate_graph_width(0, static_cast<int>(data.size()), width), calculate_graph_height(data[0], max_value, GRAPH_SCALE, height));
+    
+    //remove first item from dataset
+    data.erase(data.begin());
+
+    //Iterate though remaining items
+    for(int t = 0; t < static_cast<int>(data.size()); t++)
+        cr->line_to(calculate_graph_width(t, static_cast<int>(data.size())+1, width), calculate_graph_height(data[t], max_value, GRAPH_SCALE, height));
+    
     return true;
+}
+
+int DataFieldWidget::cb_get_selected_layer()
+{
+    const std::string val = cb_layer->get_active_id();
+    for(int i = 0; i < 5; i++)
+        if(val.compare(layer_names[i]) == 0) return i;
+   return -1;
+}
+
+float DataFieldWidget::calculate_graph_height(float data, float max, float scale, int graph_height)
+{
+    return (data / max) * scale * (float) graph_height;
+}
+
+float DataFieldWidget::calculate_graph_width(int timestep, int timesteps, int graph_width)
+{
+    return ((float) timestep / (float)timesteps)* (float) graph_width;
 }
