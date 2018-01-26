d9d372cafce8e50a106e96f2413e9a6322945718
George <george@George-Thinkpad-Debian.nopre>
2018-01-26 14:49:58 +0100

    commented dataprobe

--- src/probe/DataProbe.hpp
+++ src/probe/DataProbe.hpp
@@ -17,26 +17,60 @@ namespace renderer
 
 namespace probe
 {
-
+    /**
+     * @brief Manages the probe points and their data
+    */
     class DataProbe
     {
 
         public:
-
+            /**
+             * @brief Creates a empty data probe
+            */
             DataProbe();
+            /**
+             * @brief Creates a probe at the given coordinates
+             * @param px X Position of the probe 
+             * @param py Y Position of the probe
+             * @param data_renderer The renderer in which the probe is located
+            */
             DataProbe(float px, float py, renderer::DataRenderer* data_renderer);
             ~DataProbe();
 
             float x = 0.f;
             float y = 0.f;
 
+            /**
+             * @brief Returns the sprite which represents the visual for the probe
+             * @return The probes corresponding sprite
+            */
             sf::Sprite& getSprite();
             void fill_row(const Gtk::TreeRow& row);
 
             ProbeDetailsWindow* window = nullptr;
+            /**
+             * @brief Retrieves the values of the different properties for the probe 
+             * and inserts them in to the probe data 
+             * @param data_renderer The renderer in which the probe is located
+            */
             void fill_data(renderer::DataRenderer* data_renderer);
+            /**
+             * @brief Returns if a probe has data associated with it
+             * @return True if probe has data associated with it
+            */
             bool has_data();
+            /**
+             * @brief Gives all the probes data as a vector for the specified layer
+             * @param layer Layer for which data should be retrieved
+             * @return Vector with the probes data, or empty if no data or incorrect layer
+            */
             std::vector<float> get_all_data(int layer);
+            /**
+             * @brief Gives a single data value for a given layer and timestamp
+             * @param timestamp Time for which data should be retreved
+             * @param layer Layer for which data should be retrieved
+             * @return float with the probes data value for given time and layer, or zero if no data or incorrect layer/timestamp
+            */
             float get_data(int timestamp, int layer);
         private:
 
